07d5f60f6cd1ac60dbdc46b54363f066053a5bda
Rudigus <rodrigo.matos9@hotmail.com>
2021-07-26 09:37:00 -0300

    initial project files

---
+++ Vectore/Vectore.h
@@ -0,0 +1,18 @@
+//
+//  Vectore.h
+//  Vectore
+//
+//  Created by Rudigus on 26/07/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Vectore.
+FOUNDATION_EXPORT double VectoreVersionNumber;
+
+//! Project version string for Vectore.
+FOUNDATION_EXPORT const unsigned char VectoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Vectore/PublicHeader.h>
+
+
---
+++ VectoreTests/VectoreTests.m
@@ -0,0 +1,36 @@
+//
+//  VectoreTests.m
+//  VectoreTests
+//
+//  Created by Rudigus on 26/07/21.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface VectoreTests : XCTestCase
+
+@end
+
+@implementation VectoreTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
